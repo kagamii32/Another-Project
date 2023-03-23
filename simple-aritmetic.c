ae80174e4f5726e1850c7e8e35f12c9ae3724cd7
kagamii32 <whitematcha32@gmail.com>
2023-03-23 17:30:36 +0800

    changes 1

---
+++ simple-aritmetic.c
@@ -0,0 +1,26 @@
+/*
+Rumus Bunga
+Bunga = Saldo Terendah * (Suku bunga * Jumlah hari dalam sebulan) / 365 hari
+
+Asumsikan jika suku bunga yang digunakan tidak berubah, bernilai 5%, dan memiliki bunga sebesar 15.000
+
+*/
+
+
+#include <stdio.h>
+
+
+int main( ) {
+    int tabungan_awal, waktu, bunga;
+
+    printf("masukkan nilai tabungan awal dan suku bunga secara berturut-turut\n");
+    scanf("%d %d", &tabungan_awal, &bunga);
+    
+    int mesin = (tabungan_awal * 9.86 * bunga);
+
+    printf("jadi bunga yang didapatkan selama 10 tahun adalah sebanyak %d", mesin);
+
+
+
+    return 0;
+}
