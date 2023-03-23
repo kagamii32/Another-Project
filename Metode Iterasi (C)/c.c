b4a2e76e55a1c335cccb319a7ab9677d3b062d24
kagamii32 <whitematcha32@gmail.com>
2023-03-23 19:50:22 +0800

    Final 2

--- Metode Iterasi (C)/c.c
+++ Metode Iterasi (C)/c.c
@@ -7,16 +7,13 @@ int main() {
     const float y = 0.05; // y adalah suku bunga dalam persen (suku bunga y bernilai tetap)
     const float z = 365; // z menggambarkan seberapa lama tabungan ini disimpan (waktu)
 
-        printf("nilai tabungan awal adalah %d dan nilai suku bunga adalah %d\n", x, y);
         int a = (x * y * z)/365; // a adalah bunga dari tabungan
-        printf("Jumlah tabungan = %d\n", a);
+        printf("Jumlah bunga = %d\n", a);
 
     for (i = 1, j = 1; i <= 10, j <= 10; i++, j++) {
-    printf("Jumlah tabungan pada tahun ke %d adalah %i\n",j , i * x + a);
+    printf("Jumlah tabungan pada tahun ke %d adalah %i\n",j , i * x + a *i);
     }
     
-    printf("jadi jumlah bunga yang didapatkan dalam tabungan ialah sebesar %d", a * 10);
-
 
     return 0;
 }
